b7c4158a8e55a0b6057b8f442e282c1e8e17a864
odva7694 <odva7694@gmail.com>
2024-04-16 12:28:51 -0600

    Fix configuracion yaml de microservicios

--- MS-customer/src/main/java/com/daveone/MScustomer/MsCustomerApplication.java
+++ MS-customer/src/main/java/com/daveone/MScustomer/MsCustomerApplication.java
@@ -3,6 +3,7 @@ package com.daveone.MScustomer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MsCustomerApplication {
 
