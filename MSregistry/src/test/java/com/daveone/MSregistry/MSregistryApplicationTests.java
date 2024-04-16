e76d449b9d29cc95a1573adebee03b8fb4414fb2
odva7694 <odva7694@gmail.com>
2024-04-16 12:52:51 -0600

    Registry service added

---
+++ MSregistry/src/test/java/com/daveone/MSregistry/MSregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveone.MSregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
