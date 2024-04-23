681d9b33bbae026a21f28add2f3e800871a447f7
odva7694 <odva7694@gmail.com>
2024-04-23 15:47:03 -0600

    Adding admin server config

---
+++ MSadmin/src/main/java/com/daveone/MSadmin/MSadminApplication.java
@@ -0,0 +1,15 @@
+package com.daveone.MSadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class MSadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSadminApplication.class, args);
+	}
+
+}
---
+++ MSadmin/src/test/java/com/daveone/MSadmin/MSadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveone.MSadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
